f8284578290dc1aa26abbc4765fdd85286bb9867
BearlyKoalafied <BearlyKoalafied@users.noreply.github.com>
2017-11-16 17:35:20 +1000

    add retry error handling for error message sending

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -1,5 +1,6 @@
 import logging
 import traceback
+import time
 
 import forum_parse as fparse
 import timeout
@@ -20,10 +21,18 @@ RECOVERABLE_EXCEPTIONS = (APIException,
 
 
 def send_error_mail(reddit, message):
+    """
+    Attempt to send an error message to my main reddit account
+    Returns False on failure due to network errors
+    """
     try:
         reddit.redditor(settings.REDDIT_ACC_OWNER).message("Bot Crashed", message)
+        return True
     except RECOVERABLE_EXCEPTIONS:
         logging.getLogger((settings.LOGGER_NAME).exception("Hit exception while sending error message: "))
+        return False
+    except:
+        logging.getLogger((settings.LOGGER_NAME).exception("Hit Unexpected exception while sending error message: "))
 
 def handle_errors(reddit, func, dao):
     try:
@@ -33,10 +42,12 @@ def handle_errors(reddit, func, dao):
         dao.rollback()
     except timeout.TimeoutError:
         logging.getLogger(settings.LOGGER_NAME).exception("Hit manual Timeout exception, output: ")
-        send_error_mail(reddit, traceback.format_exc())
         dao.rollback()
+        while not send_error_mail(reddit, traceback.format_exc()):
+            time.sleep(60000)
     except:
         logging.getLogger(settings.LOGGER_NAME).exception("Hit Unexpected exception, output: ")
-        send_error_mail(reddit, traceback.format_exc())
         dao.rollback()
+        while not send_error_mail(reddit, traceback.format_exc()):
+            time.sleep(60000)
         raise
