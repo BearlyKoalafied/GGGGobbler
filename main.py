fe5eeb8f7037bfe69aeac9594be8b8c9e1c0bcc4
BearlyKoalafied <BearlyKoalafied@users.noreply.github.com>
2017-11-19 18:38:47 +1000

    Move closing code to own thread

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ def main():
 def start_main_threads(reddit):
     praw_lock = threading.Lock()
     close_event = threading.Event()
-    t_scan_reddit = threading.Timer(10, thread_scan_reddit, (reddit, close_event, praw_lock))
-    t_messages = threading.Timer(0, thread_check_msgs, (reddit, close_event, praw_lock))
+    retry_limit_event = threading.Event()
+    t_scan_reddit = threading.Thread(group=None, target=thread_scan_reddit,
+                                     args=(reddit, close_event, praw_lock))
+    t_messages = threading.Thread(group=None, target=thread_check_msgs,
+                                  args=(reddit, close_event, praw_lock))
+    t_close = threading.Thread(group=None, target=thread_wait_for_close,
+                               args=(close_event,))
     t_scan_reddit.start()
     t_messages.start()
-    try:
-        while 1:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        close_event.set()
-        raise
+    t_close.start()
 
 def secs_to_next_fraction_of_hour(n):
     """
@@ -80,6 +80,12 @@ def thread_check_msgs(r, close_event, praw_lock):
         while not close_event.is_set() and counter > 0:
             time.sleep(1)
             counter -= 1
+
+def thread_wait_for_close(close_event):
+    s = input("Type Q to end: ")
+    while s != "q" and s != "Q":
+        s = input("Type Q to end: ")
+    close_event.set()
 
 if __name__ == "__main__":
     main()
