b36103945f792bdbcb22c4173410488ae416b111
BearlyKoalafied <BearlyKoalafied@users.noreply.github.com>
2017-11-20 21:24:18 +1000

    add wait time config commands

--- config/messages.py
+++ config/messages.py
@@ -14,22 +14,20 @@ def check_messages(r):
 
 
 def process(r, body):
+    split = body.split(' ')
     if body.startswith('turn on'):
         config.set_currently_running('on')
-        send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER,
-                                  'Turning Bot on')
+        send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER, 'Turning Bot on')
         gobblogger.info("Received turn on config message")
 
     elif body.startswith('turn off'):
         config.set_currently_running('off')
-        send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER,
-                                  'Turning Bot off')
+        send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER, 'Turning Bot off')
         gobblogger.info("Received turn off config message")
 
     elif body.startswith('errmsg on'):
         config.set_error_messaging('on')
-        send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER,
-                                  'Turning Error Messaging on')
+        send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER, 'Turning Error Messaging on')
         gobblogger.info("Received errmsg on config message")
 
     elif body.startswith('errmsg off'):
@@ -37,7 +35,18 @@ def process(r, body):
         send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER,
                                   'Turning Error Messaging off')
         gobblogger.info("Received errmsg off config message")
-
+    elif len(split) == 3 and split[0] == 'waittime':
+        confirm = split[1] == 'main' or split[1] == 'msgs' or split[1] == 'manager'
+        if split[1] == 'main':
+            config.set_wait_time_main(split[2])
+        if split[1] == 'msgs':
+            config.set_wait_time_check_messages(split[2])
+        if split[1] == 'manager':
+            config.set_wait_time_manager(split[2])
+        if confirm:
+            send_confirmation_message(r, CFG_RESPONSE_MAIL_HEADER,
+                                      "Successfully configured waittime " + split[1] + " to " + str(split[2]))
+            gobblogger.info("Received waittime config message")
 
 def send_confirmation_message(reddit, header, body):
     reddit.redditor(settings.REDDIT_ACC_OWNER).message(header, body)
