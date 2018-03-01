3260a547d0572930e079d1e47317662245585133
BearlyKoalafied <BearlyKoalafied@users.noreply.github.com>
2018-03-01 22:08:03 +1000

    slight refactor and add <span> support

--- GGGGobbler/htmltomd.py
+++ GGGGobbler/htmltomd.py
@@ -5,7 +5,7 @@ def convert(html, parser='html.parser'):
     # filter out br tags because html.parser treats these incorrectly
     html = re.sub("<br\s*>", "<br/>", html)
     # remove whitespace from between tags
-    soup = BeautifulSoup(re.sub(">\s*<","><", html), parser)
+    soup = BeautifulSoup(re.sub(">\s*<", "><", html), parser)
     return process_tag(soup)
 
 
@@ -22,44 +22,35 @@ def process_tag(tag):
 
 def convert_tag(tag):
     content_inside_this_tag = process_tag(tag)
-    # tags that don't auto insert newline first
+    # tags that don't auto insert newline are processed first
     if tag.name == "a" and tag.has_attr("href"):
         return "[" + content_inside_this_tag + "]" + "(" + tag["href"] + ")"
     elif tag.name == "a":
         return content_inside_this_tag
     elif tag.name == "strong":
         return "**" + content_inside_this_tag + "**"
+    elif tag.name == "span":
+        return content_inside_this_tag
     elif tag.name == "em":
         return "*" + content_inside_this_tag + "*"
     elif tag.name == "img":
-        text = "Image Link"
-        # use alt text instead if available
-        if tag.has_attr("alt") and tag["alt"]:
-            text = tag["alt"]
-        link = tag["src"]
-        return "[" + text + "](" + link + ")"
+        return process_img(tag)
 
     # tags below here insert a newline if not already inserted by previous call to convert_tag
     if tag.name == "p":
+        output = content_inside_this_tag
+    elif tag.name == "div":
         output = content_inside_this_tag
     elif tag.name == "h2":
         output = "## " + content_inside_this_tag
     elif tag.name == "h3":
         output = "### " + content_inside_this_tag
-    elif tag.name == "div":
-        output = content_inside_this_tag
     elif tag.name == "li":
         output = "* " + content_inside_this_tag
     elif tag.name == "blockquote":
-        output = quote_boxify(content_inside_this_tag)
+        output = process_quote_box(tag)
     elif tag.name == "iframe":
-        # these are used for youtube videos.  I'll make the text something else
-        # if the src doesn't point to youtube
-        if "youtube" in tag["src"]:
-            text = "Youtube Video"
-        else:
-            text = "Embedded content"
-        output = "[" + text + "](" + tag["src"] + ")"
+        output = process_iframe(tag)
     elif tag.name == "video":
         output = "[Embedded Video](" + tag["src"] + ")"
     else:
@@ -73,6 +64,32 @@ def convert_tag(tag):
 def process_string(nav_string):
     return nav_string
 
+
+def process_img(img):
+    text = "Image Link"
+    # use alt text instead if available
+    if img.has_attr("alt") and img["alt"]:
+        text = img["alt"]
+    link = img["src"]
+    return "[" + text + "](" + link + ")"
+
+def process_quote_box(quotebox):
+    for child in quotebox:
+        if child.has_attr("class") and child["class"] == 'top':
+            # author_info = "> " + child.find("a").
+            pass
+        if child.has_attr("class") and child["class"] == 'bot':
+            return quote_boxify(process_tag(child))
+    return quote_boxify(process_tag(quotebox))
+
+def process_iframe(iframe):
+    # these are used for youtube videos.  I'll make the text something else
+    # if the src doesn't point to youtube
+    if "youtube" in iframe["src"]:
+        text = "Youtube Video"
+    else:
+        text = "Embedded content"
+    return "[" + text + "](" + iframe["src"] + ")"
 
 def quote_boxify(md):
     output = []
